be32eccea029f0d4e4faf7bf903248fdf35777c6
Yemiakintola <168643752+Yemiakintola@users.noreply.github.com>
2025-05-30 16:07:54 +0100

    Update streamlit_waec_2021_py.py

--- streamlit_waec_2021_py.py
+++ streamlit_waec_2021_py.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/11KLHKeoPnKDsN680OgfuSQqH1RYoNpSc
 """
 
-pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import joblib
